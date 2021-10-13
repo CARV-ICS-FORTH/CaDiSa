84abf490e0708f7002ec0583432cac73d9636b5a
Theocharis C. Vavouris <vavouris@ics.forth.gr>
2021-10-13 15:26:57 +0000

    fixed drop-in.sh script

--- drop-in.sh
+++ drop-in.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker exec -it -u cadisa  --env COLUMNS=`tput cols` --env LINES=`tput lines` $USER-node01 bash
+if [ "$#" -eq 0 ]; then
+    docker exec -it -u cadisa  --env COLUMNS=`tput cols` --env LINES=`tput lines` $USER-node01 bash
+else
+    docker exec -it -u cadisa  --env COLUMNS=`tput cols` --env LINES=`tput lines` $1-node01 bash
+fi
